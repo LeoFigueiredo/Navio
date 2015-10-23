e113364f63c083283ea47d9beb348211d04f5512
LeoFigueiredo <leonardofig88@gmail.com>
2015-10-22 22:30:45 -0200

    Adicionei um construtor de cópia "this->" em longitude e latitude

--- Navio/Navio/Navio.cpp
+++ Navio/Navio/Navio.cpp
@@ -3,21 +3,31 @@
 
 Navio::Navio()  //construtor
 {
-latitude = 0.0;
-longitude = 0.0;
+    this->latitude = 0.0;
+    this->longitude = 0.0;
 }
 
-Navio::Navio(float altura, float comprimento, int capacCombustivel, int passageiros, float latitude, float longitude) //construtor de copia
+Navio::Navio(float altura, float comprimento, int capacCombustivel, int passageiros, float latitude, float longitude) 
+{	
+    altura >= 0 ? this->altura = altura: this->altura = 0;	
+    comprimento >= 0 ? this->comprimento = comprimento: this->comprimento= 0;
+    capacCombustivel >= 0 ? this->capacCombustivel = capacCombustivel: this->capacCombustivel = 0;
+    passageiros >= 0 ? this->passageiros = passageiros: this->passageiros = 0;
+    latitude >= 0 ? this->latitude = latitude: this->latitude = 0;
+    longitude >= 0 ? this->longitude = longitude: this->longitude = 0;
+}
+
+Navio::Navio (const Navio &c)  //contrutor de cópia
 {
- altura >= 0 ? this->altura = altura: this->altura = 0;	
- comprimento >= 0 ? this->comprimento = comprimento: this->comprimento= 0;
- capacCombustivel >= 0 ? this->capacCombustivel = capacCombustivel: this->capacCombustivel = 0;
- passageiros >= 0 ? this->passageiros = passageiros: this->passageiros = 0;
- latitude >= 0 ? this->latitude = latitude: this->latitude = 0;
- longitude >= 0 ? this->longitude = longitude: this->longitude = 0;
+    this->altura = c.altura;
+    this->comprimento = c.comprimento;
+    this->capacCombustivel = c.capacCombustivel;
+    this->passageiros = c.passageiros;
+    this->latitude = c.latitude;
+    this->longitude = c.longitude;	
 }
 
-Navio::~Navio() //destrutor
+Navio::~Navio() //destrutor - libera o recurso
 {
 
 }
